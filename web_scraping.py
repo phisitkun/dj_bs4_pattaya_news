1db653d767f32f7023f848ea345507c93ce65c24
Phisit Kunhanate <phisitkun@gmail.com>
2023-11-15 13:30:17 +0700

    bs4 get content with URL

--- web_scraping.py
+++ web_scraping.py
@@ -1,28 +1,23 @@
-# from bs4 import BeautifulSoup
-# import requests
-
-# # HTML From File
-# with open("index.html", "r") as f:
-# 	doc = BeautifulSoup(f, "html.parser")
-
-# tags = doc.find_all("p")[0]
-
-# print(tags.find_all("b"))
-
-# # HTML From Website
-# url = "https://thepattayanews.com/2023/11/13/top-pattaya-news-from-the-last-week-pattaya-jazz-festival-soi-buakhao-road-to-be-converted-to-one-way-and-more/"
-
-# result = requests.get(url)
-# doc = BeautifulSoup(result.text, "html.parser")
-
-# prices = doc.find_all(text="$")
-# parent = prices[0].parent
-# strong = parent.find("strong")
-# print(strong.string)
-
+import requests
 from bs4 import BeautifulSoup
 
-with open("index.html","r") as f:
-    doc = BeautifulSoup(f, "html.parser")
+url = 'https://thepattayanews.com'
+response = requests.get(url)
 
-print(doc.prettify())
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    posts = soup.find_all('div', class_='td-block-row')
+    for post in posts:
+        # get content by post
+        title = post.find('h3', attrs={"class":"entry-title"}).text
+        content = soup.find('div',attrs={"class":"td-excerpt"}).text
+        links = soup.find('a',attrs={"class":"td-image-wrap"}).get('href')
+        print(title)
+        # print(content)
+        print(links)
+        print('\n\n')
+        
+else:
+    print(f"Failed to fetch content. Status code: {response.status_code}")
+
+
