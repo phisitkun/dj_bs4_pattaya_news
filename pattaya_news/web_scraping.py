71bc813a1a031c3c5d2f62bd7b21ae385dc27ffd
Phisit Kunhanate <phisitkun@gmail.com>
2023-11-23 11:04:57 +0700

    insert scraping data and check duplicate data

--- pattaya_news/web_scraping.py
+++ pattaya_news/web_scraping.py
@@ -45,6 +45,18 @@ def remove_html_tags(text):
 
 
 ############################## 
+# Function Convert Datetime
+##############################
+#
+def convert_datetimestamp(var_date):
+    date_format = "%A, %d %B %Y, %H:%M"
+    date_obj = datetime.strptime(var_date, date_format)
+    datetimestamp = date_obj.strftime("%Y-%m-%d %H:%M:%S")
+    return datetimestamp
+
+
+
+############################## 
 # Function Convert to JSON
 ##############################
 #
@@ -66,20 +78,21 @@ def convert_to_json(py_object):
 #
 def get_content_json(link):
     
-    print("get link ",total," => ",link)
     response = requests.get(link)
     soup = BeautifulSoup(response.text, "html.parser")
     title = soup.find("h1", attrs={"class":"entry-title"}).text
     short_link = soup.find("link",{"rel":"shortlink"}).get("href")
     img_url = soup.find("img", attrs={"class":"entry-thumb"})["src"]
-    entry_date = post.find("time", class_="entry-date").text.strip()
+    entry_date = soup.find("time", class_="entry-date").text.strip()
+    datetimestamp = convert_datetimestamp(entry_date)
+    # print("get entry_date ",total," => ",datetimestamp)
     
     # iframe = soup.find("div", attrs={"class":"epyt-video-wrapper"}).text
     iframe = soup.find('iframe')
-    iframe.extract() ### remove iframe tag
+    iframe.extract() ### avoid iframe tag
     
     content = soup.find_all(["h3", "p"])
-    print("get content ",total," => ",content)
+
     # Check content
     if (content != None):
         try:
@@ -105,10 +118,11 @@ def get_content_json(link):
     
     content_final = remove_html_tags(content_str)
     
-    date_format = "%A, %d %B %Y, %H:%M"
-    date_obj = datetime.strptime(entry_date, date_format)
     
-    datetimestamp = date_obj.strftime("%Y-%m-%d %H:%M:%S")
+    
+    # date_format = "%A, %d %B %Y, %H:%M"
+    # date_obj = datetime.strptime(entry_date, date_format)
+    # datetimestamp = date_obj.strftime("%Y-%m-%d %H:%M:%S")
     
     obj_data = {
             "title": title,
@@ -131,12 +145,23 @@ def get_content_json(link):
 #
 def insert_news(cursor, short_link="", title="", content="", img_url="", json={}, entry_date=datetime):
     
-    query = "INSERT INTO scraper_pagecontent (url, title, content, img_url, json, entry_date) VALUES (%s, %s, %s, %s, %s, %s) RETURNING title"
-    cursor.execute(query, (short_link, title, content, img_url, json, entry_date))
+    print("\n---------------------insert ",total,"---------------------")
+    
+    query = "INSERT INTO scraper_pagecontent (url, title, content, img_url, json, entry_date) VALUES (%s, %s, %s, %s, %s, %s) RETURNING title,url"
+    # cursor.execute(query, (short_link, title, content, img_url, json, entry_date))
+    
+    
+    try:
+        cursor.execute(query, (short_link, title, content, img_url, json, entry_date))
+    except Exception as err: ### show error detail
+        print ("Oops! An exception has occured:", err)
+        print ("Exception TYPE:", type(err))
+        
+    
     
     connection.commit()
     
-    return cursor.fetchone()[0]
+    return cursor.fetchone()
 
 
 
@@ -162,7 +187,7 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
     # posts = soup.find_all("div", class_="td-block-row")
     limit = 10 ##### add limit parameter limit result #####
-    posts = soup.find_all('h3',class_='entry-title', limit=limit)    
+    posts = soup.find_all('h3',class_='entry-title')    ### limit result (, limit=limit) 
     total = 0
     news_all = {}
 
@@ -174,40 +199,16 @@ if response.status_code == 200:
         total = total+1
         try:
             
-            # print("get link ",total," => ",link)
-            
-            
-            # title = post.find("h3", attrs={"class":"entry-title"}).text
-            # link = post.find("h3",attrs={"class":"entry-title"}).find("a").get("href")
-            # entry_date = post.find("time", class_="entry-date").text.strip()
-            
-            # print("\n Link for get contet content => ",link)
-            
             json_str = get_content_json(link)  ### return json string
             news = json.loads(json_str)    ### return object
             
-            # news = {
-            #         "title":news["title"], 
-            #         "content":news["content"], 
-            #         "img_url":news["img_url"], 
-            #         "entry_date":news["entry_date"], 
-            #         "short_link":news["short_link"],
-            #         "json_data":json_str,
-            #         }
-            
             # print("\ndisplay data before insert => ",news)
             
-            # news_title = insert_news(cursor, news["short_link"], news["title"], news["content"], news["img_url"], json_str, news["entry_date"])
-                        
-            # print("\nnew no. => ",total)
-            # print("title => ",title)
-            # print("content => ",json_str["content"])
-            # print("entry-date => ",json_str["entry_date"])
-            # # print("links => ",link)
-            # print("short_link => ",json_str["short_link"])
+            news_res = insert_news(cursor, news["short_link"], news["title"], news["content"], news["img_url"], json_str, news["entry_date"])
             
             print("\n---------------------JSON ",total,"---------------------")
-            print("\n json_str => ",json_str)  
+            print("\n insert news => ",news_res[0])  
+            print("\n short_link news => ",news_res[1])  
             
             # print("---------------------END---------------------\n\n\n")
              
